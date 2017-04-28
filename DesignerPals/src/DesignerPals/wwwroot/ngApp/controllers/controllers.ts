c9dad0a0dfb8e5c3dbd0496a95aa55873f123df3
[AntoinetteMosley] <mbagsforu@gmail.com>
2017-04-28 12:01:57 -0500

    Commit 4
    Error in Angular JS

--- DesignerPals/src/DesignerPals/wwwroot/ngApp/controllers/controllers.ts
+++ DesignerPals/src/DesignerPals/wwwroot/ngApp/controllers/controllers.ts
@@ -1,27 +1,15 @@
 namespace DesignerPals.Controllers {
 
     export class HomeController {
-        private qResource
-        public allQuestions;
+        
         public question;
-
-        public getQuestion()
-        {
-            this.allQuestions = this.qResource.query();
-        }
-        public save()
-        {
-            this.qResource.save(this.question).$promise.then(
-                () => {
-                this.question = null;
-                this.getQuestion();
-                }
-                )
-        }
+        
         constructor(private $http: ng.IHttpService)
         {
-            this.qResource = $http.get('/api/questions/:id').then(
-                (results) => { this.question = results.data; }
+           $http.get('/api/questions/:id').then(
+               (results) => {
+                   this.question = results.data;
+               }
             );
         }
     }
